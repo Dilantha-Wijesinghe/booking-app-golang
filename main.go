e5878dfbfb7abf9ed6927f9543cbf26d685a9c9c
Dilantha Wijesinghe <wdilantha66@gmail.com>
2024-01-05 20:13:34 +0530

    feat: Control flow

--- main.go
+++ main.go
@@ -1,22 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	var conferenceName = "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
-
-	fmt.Printf("conferenceName: %T, conferenceTickets: %T, remainingTickets: %T\n", conferenceName, conferenceTickets, remainingTickets)
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName) 
 	fmt.Printf("We have a total of %v tickets and only %v tickets remaining\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	for {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
+	var userTickets uint
 
 	fmt.Println("Enter your first name: ")
 	fmt.Scan(&firstName)
@@ -30,7 +33,24 @@ func main() {
 	fmt.Println("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
-	userTickets = 5
+	remainingTickets -= userTickets
+  bookings = append(bookings, firstName + " " + lastName)
+
 	fmt.Printf("Tnank You %v %v for booking %v tickets. You will recieve a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("There are %v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	firstNames := []string{}
+	for _, booking := range bookings {
+		var names = strings.Fields(booking)
+		firstNames = append(firstNames, names[0])
+	}
+
+	fmt.Printf("The first names of Bookings: %v\n" , firstNames)
+	if remainingTickets == 0 {
+		fmt.Printf("Sorry, %v is sold out!\n", conferenceName)
+		break
+	}
+	}
+	
 
 }
